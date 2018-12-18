0bfcef402f646277e7e39d1aaae9e657e8476a87
awahlber <anders.wahlberg@optum.com>
2018-12-18 15:46:34 -0600

    hey

---
+++ FlipCard.vue
@@ -0,0 +1,107 @@
+<template>
+  <div v-bind:class="flipped ? 'flip-container flipped': 'flip-container'">
+    <div class="flipper">
+      <div class="front">
+        <slot name="front"></slot>
+        <v-icon class="frontFlipBtn" 
+            v-on:click="flipped=true">
+            redo
+        </v-icon>
+      </div>
+      <div class="back">
+        <slot name="back"></slot>
+        <v-icon class="backFlipBtn" 
+            v-on:click="flipped=false">
+            redo
+        </v-icon>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FlipCard',
+  data: function() {
+    return {
+        flipped: false
+    };
+  }
+};
+</script>
+
+<style type='text/css' scoped>
+i.frontFlipBtn,
+i.backFlipBtn {
+    position:absolute; 
+    right: 20px; 
+    top: 20px;
+    color:#FFFFFF;
+}
+i.backFlipBtn {
+    -webkit-transform: rotateY(-180deg);
+    -moz-transform: rotateY(-180deg);
+    -o-transform: rotateY(-180deg);
+    -ms-transform: rotateY(-180deg);
+    transform: rotateY(-180deg);
+}
+.flip-container {
+  -webkit-perspective: 1000;
+  -moz-perspective: 1000;
+  -o-perspective: 1000;
+  perspective: 1000;
+}
+.flip-container {
+  min-height: 120px;
+}
+.flipper {
+  -moz-transform: perspective(1000px);
+  -moz-transform-style: preserve-3d;
+  position: relative;
+}
+.front,
+.back {
+  -webkit-backface-visibility: hidden;
+  -moz-backface-visibility: hidden;
+  -o-backface-visibility: hidden;
+  backface-visibility: hidden;
+  -webkit-transition: 0.6s;
+  -webkit-transform-style: preserve-3d;
+  -moz-transition: 0.6s;
+  -moz-transform-style: preserve-3d;
+  -o-transition: 0.6s;
+  -o-transform-style: preserve-3d;
+  -ms-transition: 0.6s;
+  -ms-transform-style: preserve-3d;
+  transition: 0.6s;
+  transform-style: preserve-3d;
+  top: 0;
+  left: 0;
+  width: 100%;
+}
+.back {
+  -webkit-transform: rotateY(-180deg);
+  -moz-transform: rotateY(-180deg);
+  -o-transform: rotateY(-180deg);
+  -ms-transform: rotateY(-180deg);
+  transform: rotateY(-180deg);
+  position: absolute;
+}
+.flip-container.flipped .back {
+  -webkit-transform: rotateY(0deg);
+  -moz-transform: rotateY(0deg);
+  -o-transform: rotateY(0deg);
+  -ms-transform: rotateY(0deg);
+  transform: rotateY(0deg);
+}
+.flip-container.flipped .front {
+  -webkit-transform: rotateY(180deg);
+  -moz-transform: rotateY(180deg);
+  -o-transform: rotateY(180deg);
+  -ms-transform: rotateY(180deg);
+  transform: rotateY(180deg);
+}
+.front {
+  z-index: 2;
+}
+</style>
---
+++ README.md
@@ -0,0 +1,49 @@
+### vue-flip-card
+
+<p align="center">
+  Generic flip card that allows for completely arbitrary content on each side. Slot-ified version of Karakanb's awesome `vue-info-card` component, which you can find [here.](https://github.com/andersponders/vue-info-card)
+</p>
+
+## Installation
+FlipCard uses vuetify with md-icons as the icon that you click to flip the cards, but you can put whatever you want in there instead if you dont use vuetify.
+
+## Usage
+
+Take a look at this sick example, right here boys:
+
+```html
+<template>
+  <FlipCard>
+    <template slot="front">
+      <span>Hey loser</span>
+    </template>
+    <template slot="back">
+      <span>Thought u could get rid of me loser?</span>
+    </template>
+  </FlipCard>
+</template>
+
+<script>
+import FlipCard from 'vue-flip-card';
+
+export default {
+  components: {
+    FlipCard,
+  }
+};
+</script>
+```
+### Props
+
+Throw whatever you want in those slots
+
+## Credits
+
+Addtl credits from Karakanb:
+The component is based on two main features: spark line and flip effect. The spark line is created with the amazing [vuetrend](https://github.com/QingWei-Li/vue-trend) component, which creates cool and customizable graphs. The flipping effect is implemented in pure CSS, and influenced from the tutorial [here](https://davidwalsh.name/css-flip). 
+
+## Browser Support
+The component supports all of the modern browsers, with least versions: Firefox 37, Chrome 42, Safari 8, Opera 29, and IE 10.
+
+## License
+The project is under MIT License.
